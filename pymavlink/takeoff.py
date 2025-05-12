19e5fd5d0682f09af555d3a0a34d48345bcb6b49
Lounès YOUBI <83766265+Lounes-YOUBI@users.noreply.github.com>
2025-05-12 16:13:52 +0200

    Update takeoff.py

--- pymavlink/takeoff.py
+++ pymavlink/takeoff.py
@@ -25,6 +25,8 @@ vehicle = mavutil.mavlink_connection('/dev/ttyACM0')
 vehicle.wait_heartbeat()
 print("Drone connecté")
 
+vehicle.mav.request_data_stream_send(vehicle.target_system, vehicle.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 10, 1)
+
 def is_armable():
 	""" Vérifie si le drone est armable """
 	msg = vehicle.recv_match(type='SYS_STATUS', blocking=True)
