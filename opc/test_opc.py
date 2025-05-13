885d8b9df7904b4a53c88dbbd9444cf73d9780f5
admin-drone <admindrone@pc-0162.insa-strasbourg.fr>
2025-05-13 15:24:33 +0200

    commit

---
+++ opc/test_opc.py
@@ -0,0 +1,26 @@
+from time import sleep
+import spidev
+import opcng as opc
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.mode = 1
+spi.max_speed_hz = 500000
+spi.lsbfirst = False
+
+dev = opc.detect(spi)
+
+print(f'device information: {dev.info()}')
+print(f'serial: {dev.serial()}')
+print(f'firmware version: {dev.serial()}')
+
+# power on fan and laser
+dev.on()
+
+for i in range(10):
+    # query particle mass readings
+    sleep(1)
+    print(dev.pm())
+
+# power off fan and laser
+dev.off()
