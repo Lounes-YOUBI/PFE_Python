b73b3ce058cbeda857230181efd6e18e5fb6e4ae
admin-drone <admindrone@pc-0162.insa-strasbourg.fr>
2025-05-13 16:01:49 +0200

    sds

--- opc/pm2_5.py
+++ opc/pm2_5.py
@@ -1,6 +1,9 @@
 from time import sleep
 import spidev
 import opcng as opc
+import statistics
+
+pm25_values = []
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
@@ -17,10 +20,18 @@ print(f'firmware version: {dev.serial()}')
 # power on fan and laser
 dev.on()
 
-for i in range(10):
-    # query particle mass readings
+for i in range(100):
     sleep(1)
-    print(dev.pm()["PM2.5"])
+    value = dev.pm()["PM2.5"]
+    print("PM2.5 : ", value, "µg/m^3")
+    pm25_values.append(value)
 
 # power off fan and laser
 dev.off()
+
+# Calcul de la moyenne et de l'écart type
+mean_pm25 = statistics.mean(pm25_values)
+stdev_pm25 = statistics.stdev(pm25_values)
+
+print(f"Moyenne PM2.5 : {mean_pm25:.2f}")
+print(f"Écart type PM2.5 : {stdev_pm25:.2f}")
